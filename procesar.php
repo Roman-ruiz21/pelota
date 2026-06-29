98e8ca5023cc8214c500cd34d757de22353fcdac
Roman-ruiz21 <ruizroman060709@gmail.com>
2026-06-29 16:13:09 -0300

    Update procesar.php

--- procesar.php
+++ procesar.php
@@ -1,12 +1,14 @@
 <?php
 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "bd_pelotasensor";
+$db   = "u894818569_sistemas";
+$user = "u894818569_murialdo";
+$pass = "Ilm988vb";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $conn = new mysqli($host, $user, $pass, $db);
-
+$conn->set_charset("utf8mb4");
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
